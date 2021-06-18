832a6f292bc7102e50b40a7a4f07fbf405f20715
John Hautzinger <john@hautzinger.org>
2021-06-18 15:01:25 -0700

    Initial Commit

---
+++ spec/direnv-for-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DirenvForAtomView from '../lib/direnv-for-atom-view';
+
+describe('DirenvForAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
